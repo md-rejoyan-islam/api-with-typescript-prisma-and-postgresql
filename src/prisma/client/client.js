6191bfbc9210631ca9cb893b4579b1d4016114ff
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-10-29 23:02:57 +0600

    update client data

--- src/prisma/client/client.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const client = new client_1.PrismaClient();
-exports.default = client;
